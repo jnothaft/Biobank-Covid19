3ca774c665b2ae03874853d88ae63c271eb41e98
Julia Santos Nothaft <jnothaft@bu.edu>
2021-03-15 14:14:19 -0400

    Pre-lecture 16 - Quotes model

--- biobank_database/urls.py
+++ biobank_database/urls.py
@@ -20,5 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('hw/', include('hello_world.urls')),  # new link URLS from our app
-    path('', include('pages.urls')),
+    path('pages/', include('pages.urls')),
+    path('quotes/', include('quotes.urls')),
 ]
---
+++ quotes/admin.py
@@ -0,0 +1,8 @@
+#quotes/admin.py
+from django.contrib import admin
+
+# Register your models here.
+
+from .models import Quote
+
+admin.site.register(Quote)
