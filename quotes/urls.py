7e087d60732bfe96c781f46606b7a60cffb7a7d3
Julia Santos Nothaft <jnothaft@bu.edu>
2021-03-21 22:34:16 -0400

    examples class 17 (random quote and single quote page views)

--- quotes/urls.py
+++ quotes/urls.py
@@ -1,8 +1,10 @@
 # quotes/urls.py
 
 from django.urls import path
-from .views import HomePageView
+from .views import HomePageView, QuotePageView, RandomQuotePageView
 
 urlpatterns = [
-   path('', HomePageView.as_view(), name="home_page"),
+   path('', RandomQuotePageView.as_view(), name="home_page"),
+   path('all', HomePageView.as_view(), name="all_quotes"),
+   path('quote/<int:pk>', QuotePageView.as_view(), name="quote"),
 ]
--- quotes/views.py
+++ quotes/views.py
@@ -1,5 +1,7 @@
+import random
+
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import Quote
 
 
@@ -10,3 +12,28 @@ class HomePageView(ListView):
     model = Quote  # retrieve Quote objects from the database
     template_name = "quotes/home.html"
     context_object_name = "quotes"
+
+
+class QuotePageView(DetailView):
+    """Display a single quote object"""
+    model = Quote  # retrieve Quote objects from the database
+    template_name = "quotes/quote.html"
+    context_object_name = "quote"
+
+
+class RandomQuotePageView(DetailView):
+    """Display a single quote object"""
+    model = Quote  # retrieve Quote objects from the database
+    template_name = "quotes/quote.html"
+    context_object_name = "quote"
+
+    def get_object(self):
+        """Select one quote at random for display in the quote.html templates"""
+
+        # obtain all quotes using the object manager
+        quotes = Quote.objects.all()
+        # select one at random
+        q = random.choice(quotes)
+        return q
+
+
